d97647a7757b0cce106a14e844f8eb59f28969e4
Jesús Fco Cortés <jfcr@live.com>
2020-11-14 04:03:58 -0600

    primer tarea de arrays
    completar eliminado cuando se haga clic al elemento en la lista y actualizar la lista del arreglo en javascript y la vista

---
+++ javascript/arrays/introduction/script.js
@@ -0,0 +1,64 @@
+let lista = [ "mateo", "quica hermana mala"];
+
+let agregarTexto = (e)  => {
+
+    e.preventDefault()
+    let texto = getValueOfElementById("input1");
+    cleanInputById("input1");
+    lista.push(texto);
+    console.log(lista);
+    recorrerListaForIn();
+
+    return false;
+}
+
+
+let recorrerListaForIn = () => { 
+    let html_structure = "";    
+    let index = 0;
+    for(let texto of lista) {
+        html_structure +=  '<li onclick="evento('+ index +')">' + texto + '</li>'        
+        index++;
+    }
+    console.log(html_structure)
+    setValueInTagById(html_structure, 'list-container')    
+}
+let recorrerListaWhile = () => {     
+    let index = 0;
+    while(index < lista.length) {    
+        console.log("Mi familia:", lista[index])   
+        index++;        
+    }
+    
+    // while(true) {
+    //     console.log("Mi familia:", lista[index])   
+    //     index++;
+    //     if(index >= lista.length) {
+    //         break; //Sal del bucle
+    //     }
+    // }
+}
+let recorrerListaFor = () => {    
+    // let i = 0 ----------- Declaro una variable i que va ser igual a 0
+    // i < lista.length ---- Mientra la variable i  sea menor a la longitud de la lista, vas a ejecutar la siguiente logica
+    // i++ ----------------- Cada vez que termine la logica vamos a sumar uno a la variable i
+    let html_structure = "";
+    for(let i = 0; i < lista.length; i++) {
+        html_structure +=  '<li>' + lista[i] + '</li>'        
+    }
+    setValueInTagById(html_structure, 'list-container')    
+        
+
+}
+
+
+let eliminarElemento = (index) => {
+    lista.splice(index, 1);
+}
+
+let evento = (index) => {
+    alert(lista[index])
+}
+
+// eliminarElemento(0)
+// recorrerListaForIn()
