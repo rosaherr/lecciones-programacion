fc85105d886cd810ecad7bc6f37127cb7ba1f751
Jesús Fco Cortés <jfcr@live.com>
2020-11-14 02:51:55 -0600

    template ready for use

---
+++ template/basic-functions.js
@@ -0,0 +1,13 @@
+// Aqui obtenemos el valor de un input
+function getValueOfElementById(id) {
+    return document.getElementById(id).value
+}
+
+function getNumberOfInputById(id) {
+    let value =  document.getElementById(id).value;
+    return parseFloat(value);
+}
+
+function setValueInTagById(value, id) {
+    document.getElementById(id).innerHTML = value
+}
