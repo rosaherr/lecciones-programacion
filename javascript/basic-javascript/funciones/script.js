806925ddf747b1660420fb73f3f12f82da935ce3
Francisco Cortes <jcortes@webforcehq.com>
2020-11-26 23:01:42 -0600

    FIrst mini class javascript

---
+++ javascript/basic-javascript/funciones/script.js
@@ -0,0 +1,27 @@
+function hola( ) {
+
+    console.log("hola rositaaaa")
+    
+}
+function imprimir( mensaje ) {
+
+    console.log(mensaje)
+    
+}
+
+
+let dinero = 10
+function agregar( cantidad ) {
+    dinero += cantidad    
+}
+
+hola()
+
+imprimir("rosita")
+imprimir("mateo <3")
+
+
+console.log(dinero)
+agregar(40)
+console.log(dinero)
+
