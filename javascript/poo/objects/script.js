07474230e1451f658491eee26b4e31a47d3e0a36
Jesús Fco Rodriguez <jcortes@webforcehq.com>
2020-12-04 06:00:13 -0600

    Update script.js

--- javascript/poo/objects/script.js
+++ javascript/poo/objects/script.js
@@ -14,23 +14,59 @@ var comer = () => {
 var dormir = function() {
     console.log("mimiendo")
 }
- 
 
-// const rosita = { 
-//     nombre: 'rosita',
-//     apellido: 'Herrera',
-//     edad: "18 años", 
-//     lugar_de_nacimiento: "SALTILLO, OCOSINGO",
-//     comer: function() {
-//         console.log(this.nombre +" esta comiento")
-//     },    
-//     dormir: function() {
-//         console.log( this.nombre + " esta durmiendo")
-//     },
-//     estudiar: function() {
-//         console.log( this.nombre + " esta estudiando")
-//     }
-// }
+
+function imprimirElements(element) {
+    console.log(element)
+}
+ 
+var arreglito = ['hola', 'como,', 'estas']
+console.log(arreglito[2])
+
+var imprimirElementos = (element) => {
+    console.log(element)
+}
+// AMBAS LINEAS HACEN LO MISMO 30 31
+// arreglito.forEach( imprimirElementos )
+arreglito.forEach( element => {
+    console.log(index)
+} )
+
+arreglito.forEach(
+    function(element) {
+        console.log(element)
+    }
+);
+
+//  EL FOR EACH ES UN METODO QUE TODAS LAS 
+//  VARIABLES DE TIPO ARREGLO TIENEN Y RECIBE
+//  UNA FUNCION LA CUAL SE VA A EJECUTAR 1 VEZ
+//  POR CADA ELEMENTO EN EL ARREGLO, PERO ESTA FUNCION
+//  DEBE RECIBIR UN PARAMETROS QUE ES EL ELEMENTO
+
+const rosita = { 
+    nombre: 'rosita',
+    apellido: 'Herrera',
+    edad: "18 años", 
+    lugar_de_nacimiento: "SALTILLO, OCOSINGO",
+    comer: function() {
+        console.log(this.nombre +" esta comiento")
+    },    
+    dormir: function() {
+        console.log( this.nombre + " esta durmiendo")
+    },
+    estudiar: function() {
+        console.log( this.nombre + " esta estudiando")
+    },
+    recorrerAtributos: function(logica){
+        logica(this.nombre)
+        logica(this.apellido)
+        logica(this.edad)
+        logica(this.lugar_de_nacimiento)
+    }
+
+}
+
 
 // const mateo = { 
 //     nombre: 'mateo',
@@ -58,33 +94,33 @@ var dormir = function() {
 
 
 
-class Persona {
-    constructor(nombre, apellido, edad, mesNacimiento) {
-        this.nombre = nombre
-        this.apellido = apellido
-        this.edad = edad
-        this.mesNacimiento = mesNacimiento
-        this.sexo = null             
-    }    
-    comer() {
-        console.log(this.nombre +" esta comiento")
-    }
-    dormir() {
-        console.log( this.nombre + " esta durmiendo")
-    }
-    estudiar() {
-        console.log( this.nombre + " esta estudiando")
-    }
-    getFullName() {
-        return this.nombre + " " + this.apellido
-    }    
+// class Persona {
+//     constructor(nombre, apellido, edad, mesNacimiento) {
+//         this.nombre = nombre
+//         this.apellido = apellido
+//         this.edad = edad
+//         this.mesNacimiento = mesNacimiento
+//         this.sexo = null             
+//     }    
+//     comer() {
+//         console.log(this.nombre +" esta comiento")
+//     }
+//     dormir() {
+//         console.log( this.nombre + " esta durmiendo")
+//     }
+//     estudiar() {
+//         console.log( this.nombre + " esta estudiando")
+//     }
+//     getFullName() {
+//         return this.nombre + " " + this.apellido
+//     }    
 
-}
-const mateo =   new Persona("Mateo", "Trejo", 6, 'Octubre') 
-const rosita =   new Persona("Rosita", "Herra", 15, 'MAYO')
-rosita.comer()
-rosita.nombre = "violeta"
-rosita.comer()
+// }
+// const mateo =   new Persona("Mateo", "Trejo", 6, 'Octubre') 
+// const rosita =   new Persona("Rosita", "Herra", 15, 'MAYO')
+// rosita.comer()
+// rosita.nombre = "violeta"
+// rosita.comer()
 // mateo.comer()
 
 // console.log( francisco.getOroscopo() )
